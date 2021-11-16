db72cc03ac7259a0d25d9dc74daada790ea5c6d0
JeonJieun <wjn135@naver.com>
2021-11-16 20:24:04 +0900

    guestbook

---
+++ guestbook/controllers/guestbook.js
@@ -0,0 +1,29 @@
+
+const model = require('../models/guestbook');
+
+module.exports = {
+
+    index: async function(req, res) {
+        const results = await model.findAll();
+        res.render('index', {
+            list: results || []
+        });
+    },
+
+    add: async function(req, res) {
+        const results = await model.insert(req.body);
+        res.redirect("/");
+    },
+
+    form: function(req, res) {
+        res.render('deleteform', {no: req.query.no});
+    },
+
+    delete: async function(req, res) {
+        const results = await model.delete(req.body);
+        res.redirect("/");
+    }
+
+}
+
+
